c9158f016e223e5eac2e73bd75a6ed17df59cdb1
wisper <wisper927@163.com>
2016-11-10 11:36:17 +0800

    debug shoppingCar

--- src/module/shopping/components/shoppingCar.vue
+++ src/module/shopping/components/shoppingCar.vue
@@ -12,7 +12,7 @@
 		  </thead>
 		  <tbody>
 		    <tr v-for="(x,index) in list">
-		      <td><input class="test"  type='checkbox' :value="x.goodsName" v-model="checked"></td>
+		      <td><input class="test"  type='checkbox' :value="x.totle" v-model="checked"></td>
 		      <td><img style="width:80px;height:80px" v-bind:src='x.goodsImg'></td>
 		      <td>
 		      	<p>商品名称：{{x.goodsName}}</p>
@@ -44,25 +44,21 @@ export default {
 	},
 	//定义计算价格总和方法
 	methods: {
-		checkGoods: function(){
-			//将this指向的Vue实例赋给_this,
-			var _this = this
-     		var _totle = 0;
-     		//遍历所有商品复选框
-			$(".test").each(function(index){
-				if(this.checked == true){
-					//累加计算
-					_totle += _this.list[index].totle 
-				}
-			})
-			console.log(_totle)
-			return _totle
+		getTotle: function(){
+			var _totle = 0;
+			this.checked.forEach(function(x){
+   				_totle += x
+   			})	
+   			return _totle
 		}
 	},
 	//监听checked变化
 	watch: {
 		checked: function(){
-			this.totle = this.checkGoods()
+			this.totle = this.getTotle()
+		},
+		allChecked: function(){
+			this.totle = this.getTotle()
 		}
 	},
 	//计算属性
@@ -79,7 +75,7 @@ export default {
    	 	set:function(value){
    	 		if(value){
    	 			 this.checked = this.list.map(function(x) {
-		            return x.goodsName
+		            return x.totle
 		         })
    	 		}else{
    	 			this.checked = []
